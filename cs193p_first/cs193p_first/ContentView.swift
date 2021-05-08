2ba41c970337f8c1c5ea78d810e40b0b8d36bd8a
HyeonWooJeong <qhsh9713@gmail.com>
2021-05-08 20:04:18 +0900

    add 1week project

---
+++ cs193p_first/cs193p_first/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cs193p_first
+//
+//  Created by Hyeon Woo Jeong on 2021/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs193p_first/cs193p_first/cs193p_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193p_firstApp.swift
+//  cs193p_first
+//
+//  Created by Hyeon Woo Jeong on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct cs193p_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
