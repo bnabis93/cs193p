1e618d51a8a46370e623051d0205af840990582e
HyeonWooJeong <qhsh9713@gmail.com>
2021-05-21 00:31:28 +0900

    complete week 3

--- cs193p_first/cs193p_first/EmojiMemoryGameView.swift
+++ cs193p_first/cs193p_first/EmojiMemoryGameView.swift
@@ -28,16 +28,31 @@ struct CardView : View{
     var card : MemoryGame<String>.Card
     
     var body: some View{
+        GeometryReader(content: { geometry in
+            body(for : geometry.size)
+        })
+    }
+    
+    
+    func body(for size : CGSize) -> some View {
         ZStack(content: {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius:cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             }
             else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         })
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    
+    let cornerRadius : CGFloat = 10.0
+    let edgeLineWidth : CGFloat = 3
+    func fontSize(for size : CGSize) -> CGFloat {
+        min(size.width, size.height ) * 0.75
     }
 }
 
